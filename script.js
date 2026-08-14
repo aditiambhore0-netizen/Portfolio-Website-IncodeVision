7556c90f8aae52e518e449479bb63327b3755611
Aditi Ambhore <aditiambhore0@gmail.com>
2026-08-14 21:44:07 +0530

    Add typing effect for displaying roles
    Implement typing effect for text array of roles.

---
+++ script.js
@@ -0,0 +1,35 @@
+const text = [
+"Computer Science Engineer",
+"AI Enthusiast",
+"Web Developer",
+"Problem Solver"
+];
+
+let count=0;
+let index=0;
+let currentText="";
+let letter="";
+
+(function type(){
+
+if(count===text.length){
+count=0;
+}
+
+currentText=text[count];
+letter=currentText.slice(0,++index);
+
+document.getElementById("typing").textContent=letter;
+
+if(letter.length===currentText.length){
+
+count++;
+index=0;
+
+setTimeout(type,1500);
+
+}else{
+setTimeout(type,100);
+}
+
+})();
